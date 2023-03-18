745f2bed8e368312283aed8be6fa70ee56486521
Amdalat Jinadu <babi.jinadu@gmail.com>
2023-03-18 21:10:21 +0000

    amended javascript and added if statement

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ var displayTime = now.format("h:mm:ss a");
 $("#currentDay").text(displayDate + " - " + displayTime);
 
 //timeblocks for each hour of the workday (9am-5pm) using Bootstrap classes and jQuery//
-var listOfTimes = [ "9am", "10am", "11am", "12pm"]
+var listOfTimes = [ "9 am", "10 am", "11 am", "12 pm"]
 
 for (i=0; i< listOfTimes.length; i++) {
     var row = $("<div>").addClass("row time-block");
     var hour = $("<div>").addClass("col-1 hour").text(listOfTimes[i]);
     // if statement for checking past, present, future
-    var text = $("<textarea>").addClass("col-10 future description").attr("id", listOfTimes[i]);
-    var save = $("<button>").addClass("col-1 saveBtn").html("<i class='fas fa-save'></i>");
     // current hour is gotten from moment
+    var currentTime = moment().format("H");
+    if (currentTime<parseInt(listOfTimes[i].split(' ')[0])) {
+        var text = $("<textarea>").addClass("col-10 future description").attr("id", listOfTimes[i]);
+    } else if (currentTime>parseInt(listOfTimes[i].split(' ')[0])){
+        var text = $("<textarea>").addClass("col-10 past description").attr("id", listOfTimes[i]);
+    } else {
+        var text = $("<textarea>").addClass("col-10 present description").attr("id", listOfTimes[i]);
+    }
+    
+    var save = $("<button>").addClass("col-1 saveBtn").html("<i class='fas fa-save'></i>");
+   
     // check the time in relation to the hour to get the color of the text area
     // add event listener to save on click to get the info from text area and save to local storage
     // if tehre is information in local storage show it on load
